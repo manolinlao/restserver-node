5eb92fe4ee4b473b89afcd846ce75dbe3fc39bae
Manuel Lao <manolin@iMac-de-Manuel.local>
2018-11-25 17:41:52 +0100

    uso de variable de enrorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -18,8 +18,7 @@ let urlDB;
 if(process.env.NODE_ENV==="dev"){
   urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-  urlDB= 'mongodb://cafe-user:1234cafeuser@ds115874.mlab.com:15874/cafe';
-  //urlDB = process.env.MONGO_URI;
+  urlDB = process.env.MONGO_URI;
 }
 //nos inventamos el environment URLDB que usaremos en server.js
 process.env.URLDB = urlDB;
